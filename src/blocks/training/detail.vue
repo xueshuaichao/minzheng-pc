fd80de95de8c171b9af804d67993973eb820d4f1
lily <catyumouse@sina.com>
2020-10-09 17:56:12 +0800

    feat: modal

--- src/blocks/training/detail.vue
+++ src/blocks/training/detail.vue
@@ -107,13 +107,22 @@
                 </div>
             </div>
         </template>
+        <tipModel
+            :show="showModel"
+            :scene-id="sceneId"
+            @closeing="closeing"
+        />
     </div>
 </template>
 <script>
 import api from '../../api/training';
 import learningsApi from '../../api/learnings';
+import tipModel from '../../view/components/tip-model.vue';
 
 export default {
+    components: {
+        tipModel,
+    },
     data() {
         return {
             percent: 10,
@@ -138,17 +147,28 @@ export default {
             taskId: 8,
             lackInfo: false,
             disabled: 0, // 0报名中，1.报名未开始 2报名已结束
+            showModel: false,
+            selItem: null,
+            sceneId: 0,
         };
     },
     created() {
         if (this.$route.query.id !== undefined) {
             this.taskId = Number(this.$route.query.id);
         }
-
         this.getDetail();
         this.getUserInfo();
     },
     methods: {
+        closeing(arg) {
+            this.showModel = false;
+            if (arg) {
+                // 跳转试卷。就是去考试的了
+                this.$router.push({
+                    path: `exam/detail/${this.selItem.scene.id}?examtype=${this.selItem.scene.examType}`,
+                });
+            }
+        },
         jumpRouter(item) {
             if (item.taskCourseId) {
                 this.$router.push({
@@ -158,11 +178,10 @@ export default {
                     },
                 });
             } else {
-                // 跳转试卷。就是去考试的了
-                this.$router.push({
-                    path: `exam/detail/${item.taskPaperId}`,
-                });
+                this.showModel = true;
+                this.sceneId = item.scene.id;
             }
+            this.selItem = item;
         },
         getUserInfo() {
             learningsApi.userInfo({}).then((res) => {
---
+++ src/view/components/tip-model.vue
@@ -0,0 +1,184 @@
+<template>
+    <Modal
+        v-model="show"
+        class="my-tip-model"
+        width="720"
+    >
+        <div
+            slot="header"
+            class="my-tip-header"
+        >
+            <Icon
+                type="ios-close"
+                class="my-tip-close"
+                @click="toggle(0)"
+            />
+            <img src="../../assets/images/tip-model.png">
+            考试须知
+        </div>
+        <div>
+            <p class="txt">
+                注意：进入考试后必须在<b>{{ detail.duration }}分钟</b>内提交试卷，时间截止后系统将自动交卷，中途意外退出也会计时，若异常退出，<b>请尽快再次回到考试。</b>
+            </p>
+            <ul>
+                <li>
+                    考核记录最高成绩
+                </li>
+                <li>
+                    考核标准：<span>{{ detail.totalCount }}道题{{
+                        detail.duration
+                    }}分钟</span>
+                </li>
+                <li>
+                    合格标准：<span>{{ detail.passingScore }}分及格、满分{{
+                        detail.totalScore
+                    }}分</span>
+                </li>
+                <li>
+                    考试题型：<span>{{ detail.types }}</span>
+                </li>
+            </ul>
+        </div>
+        <div
+            slot="footer"
+            class="my-modal-btn"
+            @click="toggle(1)"
+        >
+            开始考试
+        </div>
+    </Modal>
+</template>
+<script>
+import api from '../../api/training';
+
+export default {
+    props: {
+        show: {
+            type: Boolean,
+            default: true,
+        },
+        sceneId: {
+            type: Number,
+            default: 0,
+        },
+    },
+    data() {
+        return {
+            detail: {
+                questionType: [],
+                duration: 0,
+                totalScore: 0,
+                passingScore: 0,
+                totalCount: 0,
+                types: '',
+            },
+        };
+    },
+    watch: {
+        sceneId(val) {
+            if (val) {
+                this.getNote();
+            }
+        },
+    },
+    methods: {
+        toggle(arg) {
+            this.$emit('closeing', arg);
+        },
+        getNote() {
+            api.getNote({ id: this.sceneId }).then((res) => {
+                if (res.success) {
+                    let types = '';
+                    if (res.data.questionType && res.data.questionType.length) {
+                        types = res.data.questionType.join('、');
+                    }
+                    console.log(res);
+                    this.detail = {
+                        ...this.detail,
+                        ...res,
+                        duration: this.transferTime(res.data.duration),
+                        types,
+                    };
+                }
+            });
+        },
+        transferTime(time) {
+            return Math.round(time / 60);
+        },
+    },
+};
+</script>
+<style scoped lang="less">
+.my-tip-model {
+    /deep/.ivu-modal-header,
+    /deep/.ivu-modal-footer {
+        border: none !important;
+    }
+    /deep/.ivu-modal-close {
+        display: none;
+    }
+    .my-tip-close {
+        position: absolute;
+        right: 20px;
+        top: 20px;
+        font-size: 34px;
+        color: #9397aa;
+    }
+    .my-tip-header {
+        font-size: 24px;
+        color: #272f55;
+        font-weight: 500;
+        text-align: center;
+        margin-top: 20px;
+        img {
+            position: relative;
+            top: 6px;
+            left: 8px;
+        }
+    }
+    .my-modal-btn {
+        background: #d14242;
+        color: #fff;
+        font-size: 20px;
+        width: 198px;
+        height: 40px;
+        text-align: center;
+        line-height: 40px;
+        margin: 0 auto 20px;
+        border-radius: 6px;
+        cursor: pointer;
+    }
+
+    .txt {
+        color: #272f55;
+        font-size: 18px;
+        line-height: 28px;
+        padding: 0 70px;
+        b {
+            color: #d14242;
+            font-style: normal;
+        }
+    }
+    ul {
+        width: 400px;
+        margin: 0 auto;
+    }
+    li {
+        list-style: none;
+        width: 400px;
+        height: 40px;
+        line-height: 40px;
+        padding-left: 68px;
+        box-sizing: border-box;
+        background: #fef6f6;
+        border-radius: 6px;
+        color: #272f55;
+        font-size: 20px;
+        margin-top: 16px;
+        font-weight: 500;
+        span {
+            font-weight: normal;
+        }
+    }
+}
+</style>
